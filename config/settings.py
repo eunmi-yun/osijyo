4b417a01739914e4d104f4511e1adbb6fb7522e1
Wonhyun <whsung95@gmail.com>
2022-12-01 10:53:52 +0900

    save main css update

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'osijyo',
         'USER': 'root',
-        'PASSWORD': '0621',
+        'PASSWORD': '5164',
         'HOST': 'localhost',
         'PORT': '3306',
     }
