907b386f1ddc654c154aec624d54ebcc7b76ebf7
wonhyun <whsung95@gmail.com>
2022-12-06 02:21:26 +0900

    [DEV] vews.py 파일 정리
    - 불필요한 import 제거
    - filePath 변수 추가
    - model과 input shape 맞게 변경

--- confirm/views.py
+++ confirm/views.py
@@ -4,7 +4,6 @@ from django.core.files.storage import FileSystemStorage
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-from tensorflow.keras.models import load_model
 import os
 from django.conf import settings
 
@@ -29,16 +28,18 @@ def imageCreate(request):
         fs = FileSystemStorage()
         # FileSystemStorage.save(file_name, file_content)
         filename = fs.save(myfile.name, myfile)
+        
+        modelPath = os.path.join(settings.BASE_DIR, 'saved_model\\tomato_DenseNet201.h5')
+        tomato_model = tf.keras.models.load_model(modelPath) 
 
-        tomato_model =load_model(os.path.join(settings.BASE_DIR, 'saved_model/tomato_DenseNet201.h5')) 
         # tomato_model = tf.keras.models.load_model('saved_model/tomato_DenseNet201.h5')
 
         image = Image.open('media/'+myfile.name)
-        resized_image = image.resize((160,160))
+        resized_image = image.resize((224,224))
         image_arr = np.array(resized_image)
 
         # image_arr = np.array(image)
-        predictions = tomato_model.predict(image_arr.reshape(1,160,160,3))
+        predictions = tomato_model.predict(image_arr.reshape(1,224,224,3))
         print(predictions)
         # 예측값이 가장 높은 질병의 인덱스를 찾아옴
         idx = predictions[0].argmax()
@@ -46,6 +47,7 @@ def imageCreate(request):
         # 인덱스 번호를 질병명으로 바꿈.
         lables = ['세균점무늬병','겹무늬병','정상','잎곰팡이병','흰무늬병','황화 잎말림 바이러스']
         result = lables[idx]
+
         return render(request, 'confirm/result.html', {'result':result})
   
 
