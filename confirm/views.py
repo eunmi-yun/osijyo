aeacf31405a266a3c904cff75d3cbf815fd0eeb3
eunmi-yun <captn.r.dev@gmail.com>
2022-12-13 22:10:30 +0900

    result_cnn 이미지 연결

--- confirm/views.py
+++ confirm/views.py
@@ -135,11 +135,12 @@ def imageCreate(request):
             photo_list.reg_date = datetime.now()
             photo_list.photo = request.FILES['img_upload']
             photo_list.save()
+            photo_test = photo_list.photo
 
             modelPath = os.path.join(settings.BASE_DIR, 'saved_model\\tomato_DenseNet201.h5')
             tomato_model = tf.keras.models.load_model(modelPath) 
 
-            image = Image.open('media/'+myfile.name)
+            image = Image.open(photo_list.photo)
             resized_image = image.resize((224,224))
             image_arr = np.array(resized_image)
 
@@ -162,9 +163,9 @@ def imageCreate(request):
             url_result=url_lables[idx]
             result2_lables = ['하우스 내부를 청결하게 관리하고 다습하지 않도록 통풍과 환기를 잘 시킨다.','온실재배 시 내부가 다습하지 않도록 환기를 한다','정상','항상 포장을 청결히 하고 병든 잎이나 줄기는 조기에 제거하여 불에 태우거나 땅속 깊이 묻는다.','90%이상의 상대습도가 유지되지 않도록 해야 하고 통풍이 잘되게 하고 밀식하지 않는다.','종자를 선별하고, 소독하여 파종 해야 하며 재배 시 균형시비를 하고 병든 잎은 조기에 제거한다.','담배가루이 약제를 처리하여 박멸']
             result2= result2_lables[idx]
-
-            return render(request, 'confirm/result_cnn.html', {'result':result,'url_result':url_result,'result2':result2, 'photo_list.photo':photo_list.photo})
-
+            print(photo_list.photo) 
+            return render(request, 'confirm/result_cnn.html', {'result':result,'url_result':url_result,'result2':result2, 'photo_test':photo_test})
+              
     else:
         messages.warning(request, messages.warning, '이미지를 선택해 주세요!')
         return render(request,'confirm/confirm.html')
